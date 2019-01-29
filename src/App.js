a108b55b1b477519095f46fbdbe7f2c720c75967
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-01-29 03:09:06 +0300

    Started

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component,Fragment } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Naavbar"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+          <Fragment>
+            <Navbar/>
+            <h3>Hello from app</h3>
+          </Fragment>
+    )
   }
 }
 
---
+++ src/Components/Naavbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Naavbar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello navbar</h3>
+      </div>
+    )
+  }
+}
