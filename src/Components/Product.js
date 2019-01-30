38f4ec1b74a3a58cca3872192e454151fbf6488a
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-01-31 02:12:28 +0300

    Product is not referance, it is changed

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { ProductConsumer } from "../context";
+import PropTypes from "prop-types"; 
+
 export default class Product extends Component {
   render() {
     const { id, title, img, price, inCart } = this.props.product;
@@ -50,6 +52,19 @@ export default class Product extends Component {
     );
   }
 }
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    id:PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool,
+
+  }).isRequired
+}
+
+
 
 const ProductWrapper = styled.div`
   .card {
--- src/context.js
+++ src/context.js
@@ -7,8 +7,25 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state={
-    products:storeProducts,
+    products:[],
     detailProduct:detailProduct
+  }
+
+
+  componentDidMount(){
+     this.setProducts();
+  }
+
+  setProducts =()=>{
+    let products=[];
+    storeProducts.forEach(item=>{
+        const singleItem = {...item};
+        products = [...products,singleItem];
+
+    })
+    this.setState(()=>{
+      return {products}
+    })
   }
 
   handleDetail=()=>{
