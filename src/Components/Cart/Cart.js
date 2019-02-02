4e4537d03034d227987af4ea88bf6110ec1f974f
selfcamp-JM <jakhongir.mamadov@gmail.com>
2019-02-02 03:45:49 +0300

    cart component is ready except paypall

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,6 +4,7 @@ import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
 import {ProductConsumer} from "../../context";
 import CartList from "./CartList";
+import CartTotals from "./CartTotals";
 
 export default class Cart extends Component {
   render() {
@@ -18,8 +19,8 @@ export default class Cart extends Component {
                 <Title name="your" title="cart" />
                 <CartColumns />
                 <CartList value ={value}/>
+                <CartTotals value ={value} />
                 </React.Fragment>
-                
               )
             }
             else{
